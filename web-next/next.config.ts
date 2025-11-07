f48eda38374138ba0ffba25658db5f0a07e53ba0
Ivan <105873927+ib729@users.noreply.github.com>
2025-11-08 02:37:23 +0800

    update

--- web-next/next.config.ts
+++ web-next/next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "standalone",
-	allowedDevOrigins: ['stuco.ivanbelousov.com', '*.stuco.ivanbelousov.com'],
+	allowedDevOrigins: [
+		"stuco.ivanbelousov.com",
+		"*.stuco.ivanbelousov.com",
+		"http://192.168.50.43:3000",
+	],
 };
 
 export default nextConfig;
