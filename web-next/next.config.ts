aa0dc2aa2fc757e0cd72f6b81dde74d66086c5bf
Ivan Belousov <105873927+ib729@users.noreply.github.com>
2025-11-08 01:36:29 +0800

    cross-origin requests

--- web-next/next.config.ts
+++ web-next/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+	/* config options here */
+	output: "standalone",
+	allowedDevOrigins: ['stuco.ivanbelousov.com', '*.stuco.ivanbelousov.com'],
 };
 
 export default nextConfig;
